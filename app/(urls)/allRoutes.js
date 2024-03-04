2203a87da923dcadbc0a49efb680268d6ce9b93d
Bhargav-j <jeereddybhargav@gmail.com>
2024-03-04 21:23:45 +0530

    host change

--- app/(urls)/allRoutes.js
+++ app/(urls)/allRoutes.js
@@ -1,2 +1,2 @@
-export const API_URL = process.env.PORT || 'http://localhost:3000';
+export const API_URL = process.env.HOST || 'http://localhost:3000';
 export const getAllpostsAPI = `${API_URL}/api/Tickets`;
