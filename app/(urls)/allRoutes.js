60d2c542cbaa3f25d13f13a92f9f145429db5f8d
Bhargav-j <jeereddybhargav@gmail.com>
2024-03-04 21:36:40 +0530

    new build

--- app/(urls)/allRoutes.js
+++ app/(urls)/allRoutes.js
@@ -1,2 +1,3 @@
 export const API_URL = process.env.HOST
-export const getAllpostsAPI = `${API_URL}/api/Tickets`;
+// export const getAllpostsAPI = `${API_URL}/api/Tickets`;
+export const getAllpostsAPI = `https://nextjs-ticketing-app.onrender.com/api/Tickets`;
